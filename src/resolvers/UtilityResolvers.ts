50ab54b97e5301841fb4c18cbcf0e08017714a19
Frozies <52686922+Frozies@users.noreply.github.com>
2021-08-02 20:58:22 -0400

    Implemented file uploading

--- src/resolvers/UtilityResolvers.ts
+++ src/resolvers/UtilityResolvers.ts
@@ -1,7 +1,20 @@
 // const {GraphQLDateTime} = require("graphql-iso-date/dist");
+import { AWSS3Uploader } from '../lib/uploaders/s3';
+require('dotenv').config(); // Allows use of environmental variables from the .env file
+
+const s3Uploader = new AWSS3Uploader({
+    accessKeyId: process.env.S3_ACCESS_KEY_ID,
+    secretAccessKey: process.env.S3_ACCESS_SECRET_KEY,
+    destinationBucketName: 'bucket-list-bodies'
+});
 
 export const UtilityResolvers = {
     // Date: GraphQLDateTime
+
+    Mutation: {
+        singleUpload: s3Uploader.singleFileUploadResolver.bind(s3Uploader),
+        multipleUpload: s3Uploader.multipleUploadsResolver.bind(s3Uploader)
+    }
 
     /*
     * TODO: Stripe Queries
--- src/schemas/UtilitySchemas.ts
+++ src/schemas/UtilitySchemas.ts
@@ -1,8 +1,20 @@
-const {gql} = require("apollo-server");
+import { ApolloServer, gql } from 'apollo-server'
 
 const typeDef = gql`
     scalar Date
 
+    type UploadedFileResponse {
+        filename: String!
+        mimetype: String!
+        encoding: String!
+        url: String!
+    }
+
+    extend type Mutation {
+        singleUpload(file: Upload!): UploadedFileResponse!
+        multipleUpload (files: [Upload!]!): UploadedFileResponse!
+    }
+    
     type Address {
         city: String
         line1: String
